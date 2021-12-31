70f0145d34fe26d956f23d970f66ef12c2065cdc
zhangchen <chen.zhang@kikatech.com>
2021-12-31 16:13:03 +0800

    更改循环读取devicrslist

---
+++ Baes/__init__.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+# user nanme: Mr.zhang
+# data time : 2021/12/30   5:41 下午
+# project   : __init__.py.py
+# IDE       : PyCharm
+# email     :17600960626@163.com
---
+++ case/TestCase3.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+# user nanme: Mr.zhang
+# data time : 2021/12/31   11:18 上午
+# project   : Test.py
+# IDE       : PyCharm
+# email     :17600960626@163.com
